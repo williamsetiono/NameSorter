46830d7ee18027ac179e9a8a2e7d38b3ee0eb2db
wsetiono <vincentius.william.setiono@gmail.com>
2018-11-08 09:34:49 +0700

    First commit

---
+++ Sorter/Sorter/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorter
+{
+    public class Program
+    {
+
+        public static List<string> getSortedLastNameFromFullName(List<string> names, out Dictionary<string, string> pairOfNameAndLastName)
+        {
+            List<string> lastNames = new List<string>();
+            pairOfNameAndLastName = new Dictionary<string, string>();
+
+            foreach (string name in names)
+            {
+                List<string> wordsPerName = name.Split(' ').ToList();
+                int wordsCountPerName = wordsPerName.Count - 1;
+                string lastName = wordsPerName[wordsCountPerName];
+
+                lastNames.Add(lastName);
+                pairOfNameAndLastName.Add(name, lastName);
+
+            }
+
+            lastNames.Sort();
+
+            return lastNames;
+
+        }
+
+        public static void writeTextToScreenAndFile(string filePath, List<string> lastNames, Dictionary<string, string> pairOfNameAndLastName)
+        {
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(filePath))
+            {
+                foreach (var eachLastName in lastNames)
+                {
+                    string fullName = pairOfNameAndLastName.FirstOrDefault(x => x.Value == eachLastName).Key;
+                    Console.WriteLine(fullName);
+                    file.WriteLine(fullName);
+                }
+            }
+        }
+
+        public static void printFullNameBasedOnSortedLastName(List<string> lastNames, Dictionary<string, string> pairOfNameAndLastName, string outputFilePath)
+        {
+
+            if (!File.Exists(outputFilePath))
+            {
+                File.Create(outputFilePath);
+
+                writeTextToScreenAndFile(outputFilePath, lastNames, pairOfNameAndLastName);
+
+            }
+            else if (File.Exists(outputFilePath))
+            {
+
+                writeTextToScreenAndFile(outputFilePath, lastNames, pairOfNameAndLastName);
+
+            }
+
+        }
+
+        public static List<string> ReadFileAndReturnContents(string filePath)
+        {
+            List<string> contents = System.IO.File.ReadAllLines(filePath).ToList();
+
+            return contents;
+        }
+
+        public static void Main(string[] args)
+        {
+
+            List<string> names = new List<string>();
+            string inputFilePath = @"C:\Users\Administrator\Documents\Visual Studio 2013\Projects\Name Sorter\Sorter\Sorter\Files\unsorted-names-list.txt";
+            string outputFilePath = @"C:\Users\Administrator\Documents\Visual Studio 2013\Projects\Name Sorter\Sorter\Sorter\Files\sorted-names-list.txt";
+
+            Dictionary<string, string> pairOfNameAndLastName = new Dictionary<string, string>();
+
+            //read file content
+            List<string> contents = ReadFileAndReturnContents(inputFilePath);
+
+            //add file content to variable names
+            contents.ForEach(x => names.Add(x));
+
+            //get sorted last name 
+            List<string> lastNames = getSortedLastNameFromFullName(names, out pairOfNameAndLastName);
+
+            //print full name based on sorted last name
+            printFullNameBasedOnSortedLastName(lastNames, pairOfNameAndLastName, outputFilePath);
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ Sorter/UnitTestSorter/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Sorter;
+using System.Linq;
+
+namespace UnitTestSorter
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestInputContent()
+        {
+            //Arrange
+            string inputFilePath = @"C:\Users\Administrator\Documents\Visual Studio 2013\Projects\Name Sorter\Sorter\Sorter\Files\unsorted-names-list.txt";
+            List<string> expectedContents = new List<string> { "Orson Milka Iddins", "Erna Dorey Battelle", "Flori Chaunce Franzel", "Odetta Sue Kaspar", "Roy Ketti Kopfen", "Madel Bordie Mapplebeck", "Selle Bellison", "Leonerd Adda Mitchell Monaghan", "Debra Micheli", "Hailey Avie Annakin" };
+
+            //Act
+            List<string> contents = Program.ReadFileAndReturnContents(inputFilePath);
+
+            //Assert
+            CollectionAssert.AreEqual(expectedContents, contents);
+
+        }
+
+
+        [TestMethod]
+        public void TestOutputContent()
+        {
+            //Arrange
+            string inputFilePath = @"C:\Users\Administrator\Documents\Visual Studio 2013\Projects\Name Sorter\Sorter\Sorter\Files\unsorted-names-list.txt";
+            List<string> names = new List<string>();
+            Dictionary<string, string> pairOfNameAndLastName = new Dictionary<string, string>();
+            List<string> expectedOutput = new List<string> { "Hailey Avie Annakin", "Erna Dorey Battelle", "Selle Bellison", "Flori Chaunce Franzel", "Orson Milka Iddins", "Odetta Sue Kaspar", "Roy Ketti Kopfen", "Madel Bordie Mapplebeck", "Debra Micheli", "Leonerd Adda Mitchell Monaghan" };
+            List<string> output = new List<string>();
+
+            //Act
+            List<string> contents = Program.ReadFileAndReturnContents(inputFilePath);
+            contents.ForEach(x => names.Add(x));
+            List<string> lastNames = Program.getSortedLastNameFromFullName(names, out pairOfNameAndLastName);
+
+
+            foreach (var eachLastName in lastNames)
+            {
+                string fullName = pairOfNameAndLastName.FirstOrDefault(x => x.Value == eachLastName).Key;
+                output.Add(fullName);
+            }
+
+            //Assert
+            CollectionAssert.AreEqual(expectedOutput, output);
+
+        }
+
+    }
+}
